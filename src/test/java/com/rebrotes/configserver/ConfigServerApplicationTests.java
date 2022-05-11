1a9ea4d56a843d36eb56cdbd56a624229d8b2f05
rebrotescoicoi <rebrotescoicoi@gmail.com>
2022-05-10 22:48:02 -0400

    preparando primera version de un config server

---
+++ src/test/java/com/rebrotes/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebrotes.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
